7a678f2e5b9c04dcc4ab0335883f3e905dab1374
Hans <hanschung@gmail.com>
2025-12-17 14:40:33 -0800

    Add debug logging to Nango callback page

--- app/nango-callback/page.tsx
+++ app/nango-callback/page.tsx
@@ -8,20 +8,30 @@ function NangoCallbackContent() {
   const searchParams = useSearchParams()
 
   useEffect(() => {
+    console.log('Nango callback page loaded')
+    console.log('Search params:', Object.fromEntries(searchParams.entries()))
+    console.log('Has opener:', typeof window !== 'undefined' && !!window.opener)
+    console.log('Window location:', typeof window !== 'undefined' ? window.location.href : 'N/A')
+    
     // Check if we're in a popup (opened by ConnectButton)
     if (typeof window !== 'undefined' && window.opener) {
+      const connectionId = searchParams.get('connection_id') || searchParams.get('connectionId')
+      console.log('Sending message to parent, connectionId:', connectionId)
+      
       // Send success message to parent window
       window.opener.postMessage({ 
         type: 'NANGO_OAUTH_SUCCESS',
-        connectionId: searchParams.get('connection_id') || searchParams.get('connectionId')
+        connectionId: connectionId
       }, window.location.origin)
       
       // Close the popup
       setTimeout(() => {
+        console.log('Closing popup')
         window.close()
       }, 500)
     } else if (typeof window !== 'undefined') {
       // If not in a popup, redirect to onboarding
+      console.log('Not in popup, redirecting to onboarding')
       router.push('/onboarding')
     }
   }, [searchParams, router])
